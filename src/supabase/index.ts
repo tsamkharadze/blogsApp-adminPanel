02e965c2fcf73c662b8d20ab8b3e3a3f3e4249a1
tsamkharadze <samkaradze@gmail.com>
2024-12-19 19:37:53 +0400

    CHANGE CREATECLIENT

--- src/supabase/index.ts
+++ src/supabase/index.ts
@@ -3,7 +3,6 @@ import { Database } from "./supabase.types";
 
 export const supabase = createClient<Database>(
   import.meta.env.VITE_SUPABASE_URL,
-  "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InFnZGFmdWlscWZvd3VpZnFub2NnIiwicm9sZSI6InNlcnZpY2Vfcm9sZSIsImlhdCI6MTczMjA5NTQzNiwiZXhwIjoyMDQ3NjcxNDM2fQ.62PK9-FKU4kOeMdmQm731impoh4OhV2zOoKc7_Gmcpo"
-
+  import.meta.env.VITE_SUPABASE_SERVICE_ROLE_SECRET
   // import.meta.env.VITE_SUPABASE_ANON_KEY,
 );
